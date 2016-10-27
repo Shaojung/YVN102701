8f9f3c07690fc66c56a7fccff6a41eae841d9c89
shaojung <shaojung@gmail.com>
2016-10-27 19:21:41 +0800

    Use thread

--- app/src/main/java/com/example/yvtc/yvn102701/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvn102701/MainActivity.java
@@ -6,12 +6,13 @@ import android.view.View;
 import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
-    ProgressBar pb;
+    ProgressBar pb, pb2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         pb = (ProgressBar) findViewById(R.id.progressBar);
+        pb2 = (ProgressBar) findViewById(R.id.progressBar2);
     }
     public void clickShow(View v)
     {
@@ -44,4 +45,10 @@ public class MainActivity extends AppCompatActivity {
         }.start();
 
     }
+
+    public void click2(View v)
+    {
+        pb2.setProgress(pb2.getProgress() + 10);
+    }
+
 }
